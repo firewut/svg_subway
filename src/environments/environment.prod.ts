6ebc305a70163911866a4c7d509a4966414216c0
Andrey Chibisov <a.chibisov@corp.mail.ru>
2019-01-25 16:59:37 +0300

    production env updated. TODO: Move settings and themes to separate json assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,14 @@ export const environment = {
   grid_width: 40,
   grid_height: 40,
 
+  station_route_from_marker: 'A',
+  station_route_to_marker: 'B',
   line_name_font_size: 25,
   line_name_font_weight: '2em',
   line_name_grid_distance: 50,
   location_marker_radius: 30,
   location_marker_text_size: 20,
+  location_marker_opacity: .5,
   station_font_size: 15,
   station_font_weight: '1.5em',
   station_grid_distance: 80,
@@ -23,13 +26,15 @@ export const environment = {
       settings: {
         background_color: '#3a3939',
         debug_grid_color: '#fff',
-        transfer_outer_color: '#fff',
-        transfer_inner_color: '#666',
+        line_name_font_color: '#fff',
         link_under_construction_color: '#aaa',
         link_under_construction_opacity: 0.75,
-        line_name_font_color: '#fff',
+        location_marker_color: '#FF2400',
+        location_marker_text_color: '#FFF',
         station_font_color: '#fff',
         station_marker_inner_color: '#fff',
+        transfer_inner_color: '#666',
+        transfer_outer_color: '#fff',
       }
     }
   ]
